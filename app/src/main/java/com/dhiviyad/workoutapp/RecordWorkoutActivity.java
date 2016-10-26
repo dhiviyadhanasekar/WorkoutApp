6f1e5d9251ec9d26117f801cd7478eac6c434ea8
Dhiviya Dhanasekar (Personal) <dhiviyadhanasekar@gmail.com>
2016-10-26 00:20:59 -0700

    vertical layout screen1

--- app/src/main/java/com/dhiviyad/workoutapp/RecordWorkoutActivity.java
+++ app/src/main/java/com/dhiviyad/workoutapp/RecordWorkoutActivity.java
@@ -3,11 +3,11 @@ package com.dhiviyad.workoutapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class RecordWorkoutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_record_workout);
     }
 }
